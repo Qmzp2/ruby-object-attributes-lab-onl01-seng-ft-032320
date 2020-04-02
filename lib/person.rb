a22f2fe1f78c2bcd73d60abde716ec05950d8d41
Kevin Mathias <komathias1@gmail.com>
2020-04-02 00:25:42 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+
+
+class Person
+  
+  def name=(name_given)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(job_given)
+    @job = job
+  end
+  
+  def job
+    @job
+  end
+  
+end
